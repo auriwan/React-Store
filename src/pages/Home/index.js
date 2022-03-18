ad7e119ac3d9c88e9cb64958289cd7720f406ab3
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-03-18 08:02:57 +0700

    Feature-List Product dari Api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,49 +1,67 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {SafeAreaView, Alert} from 'react-native-safe-area-context';
 import {Header, ProductCard} from '../../component';
+import {useSelector} from 'react-redux';
+import axios from 'axios';
 
 const Home = () => {
-  const dummy = [
-    {
-      id: 1,
-      title: 'Product Named',
-      desc: 'Lorem',
-      price: '3000',
-      image: 'https://source.unsplash.com/1600x900/?shoes',
-    },
-    {
-      id: 2,
-      title: 'Product Named',
-      desc: 'Lorem',
-      price: '3000',
-      image: 'https://source.unsplash.com/1600x900/?shoes',
-    },
-    {
-      id: 3,
-      title: 'Product Named',
-      desc: 'Lorem',
-      price: '3000',
-      image: 'https://source.unsplash.com/1600x900/?shoes',
-    },
-  ];
+  // const dummy = [
+  //   {
+  //     id: 1,
+  //     title: 'Product Named',
+  //     desc: 'Lorem',
+  //     price: '3000',
+  //     image: 'https://source.unsplash.com/1600x900/?shoes',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Product Named',
+  //     desc: 'Lorem',
+  //     price: '3000',
+  //     image: 'https://source.unsplash.com/1600x900/?shoes',
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Product Named',
+  //     desc: 'Lorem',
+  //     price: '3000',
+  //     image: 'https://source.unsplash.com/1600x900/?shoes',
+  //   },
+  // ];
+
+  const stateGlobal = useSelector(state => state);
+  const [data, setData] = useState();
 
   const renderItem = ({item}) => (
     <ProductCard
-      title={item.title}
-      desc={item.desc}
-      price={item.price}
-      image={item.image}
+      title={item.nama_barang}
+      desc={item.deskripsi}
+      price={item.harga}
+      image={item.gambar}
     />
   );
+  useEffect(() => {
+    axios
+      .get('http://api-test.q.camp404.com/public/api/material', {
+        headers: {Authorization: `Bearer ${stateGlobal.access_token}`},
+      })
+      .then(response => {
+        let res = response.data;
+        setData(res.materials);
+      })
+      .catch(error => {
+        Alert.alert(console.log(error).toString);
+      });
+  });
 
   return (
     <SafeAreaView style={styles.page}>
       <Header title={'Home'} />
       <FlatList
         showsVerticalScrollIndicator={false}
-        data={dummy}
+        data={data}
         renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
         ListHeaderComponent={<Text style={styles.label}>List Product</Text>}
